ad435e31379d844fa721935b652bfbf850b858be
wangxiaoxiang <wangxiaoxiang@aicaigroup.com>
2019-01-19 00:25:59 +0800

    feat: init

---
+++ client/containers/Project/ImportFromRap/index.js
@@ -0,0 +1,229 @@
+import './style.scss';
+import axios from 'axios'
+import React, { PureComponent as Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Input, message } from 'antd';
+import {
+  fetchInterfaceListMenu
+} from '../../../reducer/modules/interface.js';
+const Search = Input.Search;
+@connect(state => {
+  return {
+    uid: state.user.uid + '',
+    curdata: state.inter.curdata,
+    currProject: state.project.currProject
+  },
+  {
+    fetchInterfaceListMenu
+  }
+})
+class Activity extends Component {
+  constructor(props) {
+    super(props);
+  }
+  static propTypes = {
+    match: PropTypes.object,
+    projectId: PropTypes.string,
+    fetchInterfaceListMenu: PropTypes.func
+  };
+
+  formatDeep(key) {
+    let res_body = {
+      properties: {},
+      required: [],
+      title: "empty object",
+      type: "object"
+    }
+    key.forEach(rp => {
+      let identifier =rp.identifier.split('|')[0]
+      res_body.required.push(identifier)
+      let len = rp.identifier.split('|')[1]
+      if(rp.dataType.match(/array<(.*)>/)) {
+        len = len || '1'
+        res_body.properties[identifier]={
+          items: this.formatDeep(rp.parameterList),
+          maxItems: len.split('-')[0],
+          minItems: len.split('-')[1] || len.split('-')[0],
+          type: "array"
+        }
+      } else {
+        let mock = rp.remark ? rp.remark.replace('@mock=','') : ''
+        let arr = mock && mock.indexOf('$order') > -1 ? mock.split('$order')[1].replace(/[()\'\"]/g,'').split(',') : []
+
+        let ps = {
+          description: rp.name,
+          mock: mock ? {
+            mock
+          } : undefined,
+          type: rp.dataType,
+          minLength: len ? len.split('-')[0] : undefined,
+          maxLength: len ? (len.split('-')[1] ? len.split('-')[1] : len.split('-')[0]) : undefined
+        }
+        if(arr.length > 0){
+          delete ps.mock
+          delete ps.description
+          ps.enum = arr
+        }
+        if(rp.parameterList.length === 0){
+          res_body.properties[identifier] = ps
+        } else {
+          res_body.properties[identifier] = this.formatDeep(rp.parameterList)
+        }
+
+        // res_body.properties[identifier] = rp.parameterList.length === 0 ? {
+        //   description: rp.name,
+        //   mock: remark ? {
+        //     mock: remark
+        //   } : undefined,
+        //   type: rp.dataType,
+        //   minLength: len ? len.split('-')[0] : undefined,
+        //   maxLength: len ? (len.split('-')[1] ? len.split('-')[1] : len.split('-')[0]) : undefined
+        // } : this.formatDeep(rp.parameterList)
+      }
+    })
+    return res_body
+  }
+
+  async requestRap(id) {
+    let r = await this.props.fetchInterfaceListMenu(this.props.match.params.id);
+    let folderList = []
+    r.payload.data.data.forEach(e=>{
+      folderList.push(e.name)
+    })
+
+    // console.log(r)
+    const res = await axios.get('/api/interface/rap_json?id='+id)
+    console.log('rap-data=>', res.data.data)
+    if(res.data.data){
+      message.info(`远程获取RAP数据成功`);
+    }
+    let folder = res.data.data.name
+    let catid
+    if(folderList.includes(folder)===false){
+      await axios.post('/api/interface/add_cat', {
+        desc: folder,
+        name: folder,
+        project_id: this.props.match.params.id
+      }).then(res2 => {
+        message.info(`新增接口分类[${folder}]成功`);
+        catid = res2.data.data._id
+      })
+    }
+    if(!catid){
+      message.error(`创建接口分类[${folder}]失败`);
+      return false
+    }
+    res.data.data.moduleList.forEach( e=> {
+      e.pageList.forEach(j=>{
+        j.actionList.forEach( t => {
+          if(t.requestUrl){
+            let rm = t.requestType === '1' ? 'GET' : t.requestType === '2' ? 'POST' : t.requestType === '3' ? 'PUT' : 'DELETE'
+            let createParams = {
+              catid,
+              method: rm,
+              path: t.requestUrl[0]=='/' ? t.requestUrl : `/${t.requestUrl}`,
+              project_id: this.props.match.params.id,
+              title: t.name
+            }
+            axios.post('/api/interface/add',createParams).then(res3 => {
+              console.log(`插入接口[${t.name}]模板成功`)
+              if(rm != 'GET' && rm != 'POST'){
+                message.error(`【${rm}】请求方式导入可能有问题`);
+              }
+              let interface_id = res3.data.data._id
+              let req_query = []
+              let req_body_other = {
+                properties: {},
+                required: [],
+                title: "empty object",
+                type: "object"
+              }
+              if(rm === 'GET') {
+                t.requestParameterList.forEach( rp => {
+                  req_query.push({
+                    desc: rp.name,
+                    example: rp.remark.replace('@mock=',''),
+                    name: rp.identifier,
+                    required: "1"
+                  })
+                })
+              } else {
+                t.requestParameterList.forEach(rp => {
+                  req_body_other.required.push(rp.identifier)
+                  req_body_other.properties[rp.identifier] = {
+                    description: rp.name,
+                    mock: rp.remark ? {
+                      mock: rp.remark.replace('@mock=','')
+                    } : undefined,
+                    type: rp.dataType
+                  }
+                })
+              }
+
+              // let res_body = {
+              //   properties: {},
+              //   required: [],
+              //   title: "empty object",
+              //   type: "object"
+              // }
+
+              let res_body = this.formatDeep(t.responseParameterList)
+
+              // t.responseParameterList
+              
+              let upparams = Object.assign({
+                api_opened: false,
+                catid: '',
+                desc: '',
+                id: interface_id,
+                markdown: '',
+                method: '',
+                path: '',
+                req_body_form: [],
+                req_body_is_json_schema: true,
+                req_body_other: rm === 'GET' ? undefined : JSON.stringify(req_body_other),
+                req_body_type: rm === 'GET' ? undefined : 'json',
+                req_headers: rm === 'GET' ? [] : [{name: "Content-Type", value: "application/json"}],
+                req_params: [],
+                req_query: rm === 'GET' ? req_query : undefined,
+                res_body: JSON.stringify(res_body),
+                res_body_is_json_schema: true,
+                res_body_type: 'json',
+                status: 'done',
+                switch_notice: true,
+                tag: [],
+                title: ''
+              }, createParams)
+              delete upparams.project_id
+              axios.post('/api/interface/up', upparams).then(() => {
+                message.info(`插入接口[${t.name}]成功`);
+              })
+            })
+          }
+        })
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="g-row">
+        <section className="news-box m-panel">
+          <div  className="logHead">
+            <div className="Mockurl">
+              <span>Rap Project id：</span>
+              <Search
+                placeholder="Rap project id"
+                enterButton="执行"
+                size="large"
+                onSearch={id => this.requestRap(id)}
+                />
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Activity;
---
+++ common/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  exts: [{
+    name: 'import-postman'
+  },{
+    name: 'import-har'
+  },{
+    name: 'advanced-mock'
+  },{
+    name: 'import-swagger'
+  },{
+    name: 'statistics'
+  },{
+    name: 'export-data'
+  },{
+    name: 'export-swagger2-data'
+  },{
+    name: 'import-yapi-json'
+  },{
+    name: 'wiki'
+  }]
+}
